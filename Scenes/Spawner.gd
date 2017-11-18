4a061b0f7fb600c5563f47d5cd46a0d0ef188d6a
Isaac Starkie <isaacstarkie@gmail.com>
2017-11-18 20:15:24 +0000

    Fixed spawny thingy

---
+++ Scenes/Spawner.gd
@@ -0,0 +1,22 @@
+extends Node
+export (PackedScene) var Box
+onready var ref = load (Box.get_path())
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+func spawn_obstacle():
+	var box = ref.instance()
+	get_parent().add_child(box)
+	box.translate(Vector3((randf() * 8) - 4,3,0))
