2f14454348ad626acdeb152251d3b6fbc56b75c3
DC <dcdolphy@gmail.com>
2017-11-18 16:36:34 +0000

    Adding input to the player, movement now possible!

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -12,8 +12,8 @@ const MAX_SPEED = 800
 const JUMP_FORCE = 1150
 
 var direction = 0
-var impulse = Vector3(-1,0,0)
-var postest = Vector3(0,0,0)
+var impulse = Vector3(0,0,0)
+var postest = Vector3(1,1,0)
 var input_direction = 0
 var velocity = Vector2(0, 0)
 var grounded = false
@@ -21,22 +21,23 @@ var grounded = false
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	set_axis_lock(3)
 	set_process(true)
 
 func _process(delta):
 	if Input.is_action_pressed("ui_left"):
-		input_direction = -1
+		impulse = Vector3(-1, 0, 0)
 	elif Input.is_action_pressed("ui_right"):
-		input_direction = 1
+		impulse = Vector3(1, 0, 0)
 	else:
-		input_direction = 0
+		impulse = Vector3(0,0,0)
 	
 	if input_direction != 0: # If there is x-axis input
 		# Increase x-speed up to a maximum for both directions
 		velocity.x += ACCELERATION * direction * delta
 		velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
 		
-	#apply_impulse(postest, impulse)
+	apply_impulse(postest, impulse)
 	
 	
 	
