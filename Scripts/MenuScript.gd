55219fa1a71b9b953ce85767a37eaa9ded87fe5a
Daniel Cumbor <dcdolphy@gmail.com>
2018-07-25 19:57:52 +0100

    Added main scene.

--- Scripts/MenuScript.gd
+++ Scripts/MenuScript.gd
@@ -1,12 +1,9 @@
 extends VBoxContainer
 
-func _ready():
-	pass
-
 # Connected to the start button via a signal. Changes scene once pressed signal is sent.
 func _on_StartButton_pressed():
-	get_tree().change_scene("res://Scenes/Main.tscn")
+	get_tree().change_scene("res://Scenes/Main.tscn") # Change the scene using the asset path.
 
 # Quits the game once the pressed signal is sent.
 func _on_QuitButton_pressed():
-	get_tree().quit()
+	get_tree().quit() # Quit the game.
